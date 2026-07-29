04061a514577164ab89580db13484b8acd8123ec
George Malak <georgmalak2004@gmail.com>
2026-07-30 01:26:54 +0300

    fix error !

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from ultralytics import YOLO
 
-PTH_TO_MODEL  = "weights\\best.pt"
+PTH_TO_MODEL  = "weights/best.pt"
 
 @st.cache_resource
 def load_yolo_model(model_path: str = PTH_TO_MODEL):
